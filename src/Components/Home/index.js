18d49ea0d022ea6cc7e2890ce12f6e63878c2570
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-12-12 21:51:37 +0530

    Added UI of Home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import {Link} from 'react-router-dom'
 import SmallCard from "../smallCard";
 import Navbar from "../Navbar/Navbar";
+
 import "./index.css";
 
 const Home = (props) => {
   return (
     <div className="homeMain">
       <Navbar />
-      <div className="Other"></div>
+      <div className="Other">
+        <div className="left-other">
+          <h2>Travel</h2>
+          <h2>Discover</h2>
+          <p>If you're looking for an inspirational and authentic holiday experience that's tailored to your needs, you've come to the right place. We've been at the forefront of tourism to Iceland, Lapland and the Polar regions for 37 years and are proud to be one of the UK's leading independent specialist holiday companies.</p>
+          <Link to='/'>Get Started <i className="fas fa-arrow-right"></i></Link>
+        </div>
+        <div className="right-other">
+          <img src="https://cdn.britannica.com/58/124658-050-28314DA4/Maharaja-Palace-Mysuru-Karnataka-India.jpg" alt="Mysore"/>
+        </div>
+      </div>
       <div className="BottomImageBar">
         <SmallCard />
         <SmallCard />

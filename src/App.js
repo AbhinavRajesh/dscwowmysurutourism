9659fb0a8f4b3fe0cfecddda0aeab0c78f517eb4
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-12-12 16:01:13 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import "./App.css";
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Page } from "framer";
 
 function App() {
   return <Page momentum />;
 }
 
-export default App;
+export default App; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return (
+//   <Router>
+//     <Route path="/" exact component={Navbar} />
+//   </Router>)

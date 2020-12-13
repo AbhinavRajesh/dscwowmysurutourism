1f4051380dc61c9318502033418e7e92e484a335
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-12-13 11:05:56 +0530

    Added extra stuff to landing page

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SmallCard from "../smallCard";
 import Navbar from "../Navbar/Navbar";
-import Image1 from "../../assets/img/1.jpg";
-import Image2 from "../../assets/img/2.jpg";
 import { useState } from "react";
 
 import "./index.css";
 import { motion } from "framer-motion";
+
+import Image1 from "../../assets/img/1.jpg";
+import Image2 from "../../assets/img/2.jpg";
+import mysoreCard from "../../assets/img/homeCard.png"
+import mysoreMap from "../../assets/img/MysoreMap.png"
 
 const Home = (props) => {
   const [presentImage, setPresentImage] = useState(
@@ -115,6 +118,56 @@ const Home = (props) => {
             style={selectedImage === 3 ? true : null}
           />
         </motion.div>
+      </div>
+      <div className="homeSecondary">
+        <div className="homeLeftContainer">
+          <div className="homeimgbackground"></div>
+          <img src={mysoreCard} alt="" className="bg1" />
+        </div>
+        <div className="homeRightContainer">
+          <h3>MYSORE</h3>
+          <p>The word Mysore is derived from the word "mahishur" or "Mahishasurana Ooru", which means the town of Mahishasura in Kannada, the local language. This heritage city of Karnataka is famous for its splendid palaces, sandalwood and silk, but the Mysore Palace's beauty and splendour make it stand out.</p>
+          <p>Mysuru is an important manufacturing and trading centre, and it has textile (cotton and silk), rice, and oil mills, sandalwood-oil and chemical factories, and tanneries.</p>
+          <p>From 1399 until 1947 Mysore was ruled by the Wodeyar dynasty except for a brief period in the late 18th century when Hyder Ali and Tipu Sultan were in power.</p>
+          <p>Today Mysuru is a modern city that has managed to retain its quaint old world charm.</p>
+        </div>
+      </div>
+      <div className="homeSecondary">
+        <div className="homeRightContainer">
+          <h3>CULTURE</h3>
+          <p>Kannada is a Dravidian language spoken by the majority of the population and is the official state language. Languages such as Tamil and Telugu are spoken along the borders of the state. Rice is the principal food crop of the state.</p>
+          <p>Karnataka possesses a rich cultural heritage that has fos tered various religions and philosophies which have influ enced literature, architecture, folklore, music, painting, and other arts. The theatre culture is the of the most common traditions of the Kannadigas. Another common tradition among the Kannadigas during special occasions like Duss hera and Maha Shivratri, stories and devotions or vratha are sung or narrated by the devotees.</p>
+          <p>Karnataka is the land of fiestas and festivals. Numerous art and cultural festivals are hosted throughout the year. The ritual dances of Karnataka are known as Kunitha. It is a pop ular dance form accompanied by singing and the beats of decorated drums. This dance is primarily performed by men from the shepherd community.</p>
+          <p>Karnataka is the country's largest silk-producing state. It has played a pivotal role historically in the development of mul berry silk and subsequently silk sarees that are particular to both its geography and culture.</p>
+        </div>
+        <div className="homeLeftContainer">
+          <img src={mysoreCard} alt="" className="bg2" />
+          <div className="homeimgbackground"></div>
+        </div>
+      </div>
+      <div className="homeSecondary">
+        <div className="homeLeftContainer">
+          <div className="homeimgbackground"></div>
+          <img src={mysoreCard} alt="" className="bg1" />
+        </div>
+        <div className="homeRightContainer">
+          <h3>LANGUAGE</h3>
+          <p>The official language of the state of Karnataka is Kannada. However other languages like Urdu, Telugu, Tamil, Malayalam, Marathi, Tulu, Konkan and Hindi are also spoken. Most educated people use English and Hindi language too</p>
+        </div>
+      </div>
+      <div className="homeSecondary">
+        <div className="homeRightContainer">
+          <h3>FESTIVALS</h3>
+            <p>Karnataka is marked by some of the most colorful festivals. Mysore Dussehra is organized as Nada Habba and this is the main festival of Mysore.</p>
+            <p>The second important festival of Kar nataka is Ugadi (Kannada New Year). Kambala is an epic buffalo race which is an annual two-day festival, kambala season generally starts in November and lasts until March.</p>
+        </div>
+        <div className="homeLeftContainer">
+          <img src={mysoreCard} alt="" className="bg2" />
+          <div className="homeimgbackground"></div>
+        </div>
+      </div>
+      <div className="mysoreMap">
+        <img src={mysoreMap} alt="Mysore Map"/>
       </div>
     </motion.div>
   );
